ed6b3930854b52a5e316fcb78f00cac568b7447b
nicobellanich <nicolasbellanich@gmail.com>
2025-07-20 19:46:44 -0300

    add config to load enviroment

--- cmd/server/wire.go
+++ cmd/server/wire.go
@@ -2,10 +2,14 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/nicobellanich/migroblogging-platform/config"
 )
 
 func wire() http.Handler {
 	mux := http.NewServeMux()
+
+	config.Load()
 
 	return mux
 }
